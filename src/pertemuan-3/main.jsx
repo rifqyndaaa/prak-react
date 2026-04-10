1c9585891cef8644265b82faba90f6d3b668b145
rifqi24si@mahasiswa.pcr.ac.id <yanda24si@mahasiswa.pcr.ac.id>
2026-04-10 16:16:23 +0700

    p3 pt2

--- src/pertemuan-3/main.jsx
+++ src/pertemuan-3/main.jsx
@@ -1,9 +1,14 @@
 import { createRoot } from "react-dom/client";
 import Tailwindcss from "./TailwindCSS";
 import './tailwind.css';
+import UserForm from "./UserForm";
+import HitungGajiForm from "./HitungGajiForm";
+
 createRoot(document.getElementById("root"))
     .render(
         <div>
-             <Tailwindcss/>
-        </div>
-    )
+            { /* <Tailwindcss/> */ }
+        {/* <UserForm/> */}
+          <HitungGajiForm/>
+        </div>,
+    );
